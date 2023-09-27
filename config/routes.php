144e54feab755d64daeca2d82b2740818e3200a8
Rosa <xenco34@gmail.com>
2023-09-27 12:58:41 +0200

    routes

--- config/routes.php
+++ config/routes.php
@@ -1,14 +1,26 @@
 <?php 
 
 /**
- * Used to define the routes in the system.
+ * Se utiliza para definir las rutas en el sistema.
  * 
- * A route should be defined with a key matching the URL and an
- * controller#action-to-call method. E.g.:
+ * Una ruta debe definirse con una clave que coincida con la  URL y un
+ * controlador#método acción-para-llamar. P.ej.:
  * 
  * '/' => 'index#index',
  * '/calendar' => 'calendar#index'
  */
+// !Lo saco del array routes  AVER K PASA
+ 
+ 	//'/ruta' => 'test#ruta',
+//'/test' => 'test#index'
+//'/' => 'ToDo#index',
+
 $routes = array(
-	'/test' => 'test#index'
+	'/' => 'ToDo#index',
+	'/index' => 'ToDo#index',
+	'/hello' => 'ToDo#hello',
+	'/ruta' => 'test#ruta',
+	'/test' => 'test#index',
+	'/createTask' => 'ToDo#createTask'
+	
 );
