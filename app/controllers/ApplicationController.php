de9fd2e03ed7ac2a27a8f0cab779ae18e4d8889d
Rosa <xenco34@gmail.com>
2023-09-29 12:18:32 +0200

    No funciona como debe

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -16,9 +16,11 @@ class ApplicationController extends Controller
     public function getAllTasksAction()
     {
         
+        $dataJson = new TaskModel();
+        $dataJ= $dataJson->getAllTasks(); 
+       
+        
+        include ROOT_PATH . '/app/views/scripts/application/getAllTasks.phtml';
+        $this->view->dataJ = $dataJ;
     }
 }
-   
-
-
-
--- app/models/TaskModel.php
+++ app/models/TaskModel.php
@@ -2,14 +2,20 @@
 
 class TaskModel {
     protected $tasks=[];
+    protected $jsonPath;
     
+
+    public function __construct(){
+        $this->jsonPath= ROOT_PATH . '/app/models/data/DDBB.json';
+    }
     public function getAllTasks(){
     // $data= file_get_contents('/..app/models/data/DDBB.json') ;
-    $data= file_get_contents(ROOT_PATH . '/app/models/data/DDBB.json') ;
+    $data= file_get_contents($this->jsonPath) ;
     $tasks= json_decode($data, true);
     return $tasks;
 
     }
+    
     }
     
 
